0f7e55bb9f58f16070d49d1bfaeae4f20a679178
K1la <hamzaevalik2015@yandex.ru>
2025-07-15 14:41:50 +0300

    task23

---
+++ task23/task23.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	k := 7
+	s := deleteK(nums, k)
+	fmt.Printf("AFTER\tlen=%v\tcap=%v\ns=%v\n", len(s), cap(s), s)
+}
+
+func deleteK(nums []int, k int) []int {
+	if k < 0 || k >= len(nums) {
+		return nums
+	}
+
+	copy(nums[k:], nums[k+1:])
+	return nums[:len(nums)-1]
+}
