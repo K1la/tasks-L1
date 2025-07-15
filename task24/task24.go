9397b1126f87e42e33db5137fb848b712afbf555
K1la <hamzaevalik2015@yandex.ru>
2025-07-15 14:49:37 +0300

    task24

---
+++ task24/task24.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Point struct {
+	x, y float64
+}
+
+func NewPoint(x, y float64) *Point {
+	return &Point{x, y}
+}
+
+func (p *Point) Distance(other *Point) float64 {
+	dx := other.x - p.x
+	dy := other.y - p.y
+	return math.Hypot(dx, dy)
+}
+
+func main() {
+	p1 := NewPoint(1, 2)
+	p2 := NewPoint(3, 4)
+
+	d := p1.Distance(p2)
+
+	fmt.Printf("p1: (%.2f, %.2f)\n", p1.x, p1.y)
+	fmt.Printf("p2: (%.2f, %.2f)\n", p2.x, p2.y)
+	fmt.Printf("Расстояние между p1 и p2 = %.8f\n", d)
+}
